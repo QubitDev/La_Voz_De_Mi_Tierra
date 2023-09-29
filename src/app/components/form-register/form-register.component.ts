ac7ea0086e85f794eaa13cc523d59fb466889c5e
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-09-29 19:41:09 -0400

    feat: Crear FileChosser

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -7,6 +7,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./form-register.component.css']
 })
 export class FormRegisterComponent {
+
+  
+  onAudioFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const audioFile = inputElement.files?.[0];
+  
+    if (audioFile) {
+      // Aquí puedes realizar acciones con el archivo de audio, como cargarlo o mostrar información
+      console.log('Archivo de audio seleccionado:', audioFile.name);
+      // Puedes acceder a otras propiedades del archivo, como audioFile.type, audioFile.size, etc.
+    }
+  }
+
+  onTextFileSelected(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const file = inputElement.files?.[0]; // Obtener el primer archivo seleccionado
+  
+    if (file) {
+      // Aquí puedes realizar acciones con el archivo, como cargarlo o mostrar información
+      console.log('Archivo seleccionado:', file.name);
+      // Puedes acceder a otras propiedades del archivo, como file.type, file.size, etc.
+    }
+  }
+  
+
+
+
+
   contactForm!: FormGroup;
 
   constructor(private readonly fb: FormBuilder){}
