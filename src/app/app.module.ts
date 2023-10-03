13e28fbb995465609e6c73dfb3023520c2546ff4
Miguel Rodriguez <miguelrodriguezrobles23@gmail.com>
2023-10-03 00:36:25 -0400

    feat:crear HomeUsuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { Home1Component } from './home1/home1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home1Component
   ],
   imports: [
     BrowserModule,
