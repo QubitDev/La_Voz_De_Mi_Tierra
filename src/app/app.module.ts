de35719d1465a82bd7b5de590a35b8f8e1be0da8
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2023-10-03 02:49:04 -0400

    feat: Realizar merge entre la rama main la rama vistaAdm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// ...
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-<<<<<<< HEAD
-import { VistaAdmComponent } from './vista-adm/vista-adm.component';
-=======
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environments.prod';
->>>>>>> main
+import { VistaAdmComponent } from './vista-adm/vista-adm.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +20,11 @@ import { environment } from 'src/environments/environments.prod';
     AppRoutingModule,
     SharedModule,
     AngularFireModule.initializeApp(environment.firebase)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
